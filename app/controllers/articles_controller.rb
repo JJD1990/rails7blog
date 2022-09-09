8729218fa613db3426d4cae001e8595bf4cbd79a
jack douglas <jackdouglas@jacks-MacBook-Pro.local>
2022-09-09 08:55:40 +0100

    create action started

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,13 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+    def create
+        @article = Article.new
+        if @article.save
+            redirect_to articles_path
+        else
+            render :new
+        end
+    end
 end
